c2c023665623b6af3c98106458cd902e50b960d6
darshan satani <141927663+Darshanpatel001@users.noreply.github.com>
2024-01-15 12:12:30 +0530

    Update string.c

--- string.c
+++ string.c
@@ -7,14 +7,14 @@
     int i,j;
  
     printf("Enter a string: ");
-	scanf("%[^\n]s",s);
+	scanf("%s\n",s);
 
  
     j=0;
     for(i=(strlen(s)-1); i>=0;i--)
         re[j++]=s[i];
      
-    re[j]='\0';
+    re[j]='0';
  
     printf("\nOriginal String is: %s",s);
     printf("\nReversed String is: %s",re);
